7665d46bc372bf5e4aaf90adbf6f7a18554e9ec5
Huzaifa Ahmed <ahmedhuzaifa13@gmail.com>
2022-01-09 17:43:43 +0500

    Maintain state for theme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import AppBanner from "./components/AppBanner";
 import StepsSection from "./components/StepsSection";
 
 function App() {
+  // Maintain state for theme
+  useEffect(() => {
+    let htmlClasses = document.querySelector("html").classList;
+    if (localStorage.theme === "dark") {
+      htmlClasses.add("dark");
+    }
+  }, []);
+
   return (
     <div className="h-screen">
       <Navbar />
